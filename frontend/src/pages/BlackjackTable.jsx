9f72eedbffaede50a8e0322d9b9710ac00c66dc4
HuanNguyen-Dev <hnguy138@outlook.com>
2026-04-21 23:19:07 +1000

    feature: added blackjack table img, updated relevant CSS, starting to implement blackjack table logic

--- frontend/src/pages/BlackjackTable.jsx
+++ frontend/src/pages/BlackjackTable.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
+import blackjackTableIMG from '../assets/blackjack-table.png'
+import { useEffect, useState } from 'react'
+
 
 function BlackjackTable() {
-  return (
-    <div>
-      <h1>Blackjack Table</h1>
-      {/* Your blackjack table UI here */}
-    </div>
-  );
+
+    const [cards, setCards] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    useEffect(() => {
+        const fetchCards = async () => {
+            try {
+                // Fetch two decks of cards
+                const deckRes = await fetch('https://deckofcardsapi.com/api/deck/new/?deck_count=2&shuffle=true');
+                const deckData = await deckRes.json();
+                const deckId = deckData.deck_id;
+
+                // fetch the entire deck
+                const cardsRes = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=104`);
+                const cardsData = await cardsRes.json();
+
+                // Set the cards data to state
+                setCards(cardsData.cards);
+                setIsLoaded(true);
+
+                // Maybe send hash of deck to solidity and store it in blockchain
+
+                
+            } catch (error) {
+                // handle gracefully later
+                console.error('Error fetching cards:', error);
+            }
+        };
+
+        fetchCards();
+    }, []);
+
+
+    return (
+        <section className="blackjack-hero">
+            {/* <h1 className="table-title">Blackjack Table</h1> */}
+            <img src={blackjackTableIMG} alt="Blackjack Table" className="table-image" />
+        </section>
+    );
 }
 
 export default BlackjackTable;
