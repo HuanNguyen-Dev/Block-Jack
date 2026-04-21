e4c2939a107442c07360290406fb2e92ae9ce93a
HuanNguyen-Dev <hnguy138@outlook.com>
2026-04-21 16:06:20 +1000

    fix: cards now move out of deck when hovered

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,6 +34,10 @@ function App() {
     fetchCards();
   }, []);
 
+  function isHovered(index){
+    return hoveredCard === index;
+  }
+
   return (
     <>
       <section id="center-index" style={{ backgroundImage: `url(${heroImg})` }}>
@@ -41,21 +45,29 @@ function App() {
           <span>Play</span>
         </button>
         <div className="cards-container">
-          <div className="cards" style={{ transform: 'translateY(-80px)' }}
+          <div className="cards"
                 onClick={() => setisFanned(!isFanned)}>
             {isLoaded ? (
               cards.map((card, index) => {
                 const middleIndex = Math.floor(cards.length / 2);
                 const angle =  isFanned ? (index - middleIndex) * 0.8 : 0;
                 const horizontalTranslation = isFanned ? (index - middleIndex) * 20 : 0;
+                // move x depending on left or right of fan
+                const hoverX = (index - middleIndex) * 2; 
                 return (
                   <div
                     key={card.code}
                     className="card"
                     onClick={() => setIsSelectedCard(cardSelected === index ? null : index)}
+                    onMouseEnter={() => setHoveredCard(index)}
+                    onMouseLeave={() => setHoveredCard(null)}
                     style={{
-                      transform: `rotate(${angle}deg) translateX(${horizontalTranslation}px) scale(1.2) translateY(-30px)`, 
-                      zIndex: isFanned ? index : cards.length - index,
+                      transform: `rotate(${angle}deg) 
+                      translateX(${(isHovered(index)? hoverX : 0 )+ horizontalTranslation   }px) 
+                      scale(${isHovered(index) ? 1.3 : 1.2}) 
+                      translateY(${isHovered(index) ? -130 : -100}px)`, 
+                      zIndex: hoveredCard === index ? cards.length + 1 : index,
+                      transition: 'transform 0.25s ease',
                     }}
                   >
                     <img src={card.image} alt={card.code} />
