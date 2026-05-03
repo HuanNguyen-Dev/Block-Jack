1b838f265f1d8523e968a7941ccc70415682d8a2
HuanNguyen-Dev <hnguy138@outlook.com>
2026-05-03 20:00:08 +1000

    Feature: Added deposit functionality

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,8 +4,9 @@ contract Vault {
     // owner
     // for each address assign a uint256 (bal)
     mapping(address => uint256) balances;
-    // add logging e.g events for deposit, withdraw, payout
 
+    // add logging e.g events for deposit, withdraw, payout
+    event Deposit(address user, uint256 amount);
     // modifier for only the owner
     address public owner;
     modifier onlyOwner() {
@@ -14,14 +15,17 @@ contract Vault {
     }
     // constructor to assign the owner
 
-    constructor() {
-        owner = msg.sender;
+    constructor() payable {
+        // payable constructor can receive ether
+        owner = payable(msg.sender);
     }
 
     // deposit function extern payable
-    function deposit() public payable {
+    function deposit() external payable {
         require(msg.value > 0, "Deposit amount must be > 0");
         balances[msg.sender] += msg.value;
+
+        emit Deposit(msg.sender,msg.value);
     }
 
     function getContractBalance() public view returns (uint256) {
@@ -32,4 +36,7 @@ contract Vault {
     // payout functrion (only owner) - takes player and amount
 
     // receive() payable due to deposit being a payable function
+    receive() external payable {
+    revert("Use deposit()");
+}
 }
